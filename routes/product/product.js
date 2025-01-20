9a98f0bc4ac87fee1e5c8210de0bf9d40c21c3d0
mohammed mishal <michum515@gmail.com>
2025-01-20 12:48:50 +0530

    product list web

--- routes/product/product.js
+++ routes/product/product.js
@@ -3,7 +3,7 @@ import productCreate from "./api/create.js";
 import productDelete from "./api/delete.js";
 import productGet from "./api/get.js";
 import productUpdate from "./api/update.js";
-import productDetail from "./api/detail.js";
+import products from "./api/detail.js";
 
 const productRouter = express.Router();
 
@@ -11,6 +11,6 @@ productRouter.use("/create", productCreate);
 productRouter.use("/delete", productDelete);
 productRouter.use("/list", productGet);
 productRouter.use("/edit", productUpdate);
-productRouter.use("", productDetail);
+productRouter.use("", products);
 
 export default productRouter;
