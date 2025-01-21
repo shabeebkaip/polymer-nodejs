226689e8d98396f8ac1fe0f51872a35a07bcc85f
shabeebramz <shabeeb.k@ramzaula.com>
2025-01-21 15:38:04 +0300

    data fix

--- routes/subCategory/api/subCategoryName.js
+++ routes/subCategory/api/subCategoryName.js
@@ -4,19 +4,19 @@ import SubCategory from "../../../models/subCategory.js";
 const subCategoryName = express.Router();
 
 subCategoryName.get("", async (req, res) => {
-    try {
-        const categories = await SubCategory.find({});
-        res.status(200).json({
-            status: true,
-            data,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: false,
-            message: "Internal server error",
-        });
-    }
+  try {
+    const categories = await SubCategory.find({});
+    res.status(200).json({
+      status: true,
+      data: categories,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: false,
+      message: "Internal server error",
+    });
+  }
 });
 
 export default subCategoryName;
