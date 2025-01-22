a4efd41949e005277ac0bc8de9671e702da193ff
mohammed mishal <michum515@gmail.com>
2025-01-22 12:13:35 +0530

    enquiry list

--- models/brand.js
+++ models/brand.js
@@ -4,25 +4,7 @@ import { Schema } from "mongoose";
 const schema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  supplier: { type: String, required: true },
-  identification: [
-    {
-      title: { type: String, required: false },
-      value: { type: String, required: false },
-    },
-  ],
-  features: [
-    {
-      title: { type: String, required: false },
-      value: { type: String, required: false },
-    },
-  ],
-  applications: [
-    {
-      title: { type: String, required: false },
-      value: { type: String, required: false },
-    },
-  ],
+  owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
   logo: { type: String, required: false },
   cover: { type: String, required: false },
   mail: { type: String, required: false },
