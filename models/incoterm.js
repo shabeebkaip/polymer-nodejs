e2b525fd4498065ada2745bd82eeec9ebb0f4047
Ayisha Nidha P P <ayishanidha2002@gmail.com>
2025-05-09 11:49:02 +0530

    update status

--- models/incoterm.js
+++ models/incoterm.js
@@ -4,7 +4,7 @@ import { Schema } from "mongoose";
 const schema = new Schema(
   {
     name: { type: String, required: true },
-    description:{
+    fullForm:{
       type: String,
       required: true
     },
